9db13f5e099af238810ceaea4025db9ebfff7b2e
Affan <49243518+affanmohammed@users.noreply.github.com>
2024-06-17 22:45:51 +0530

    commit1

---
+++ calculator.py
@@ -0,0 +1,21 @@
+
+while True:
+    a = int(input("enter num 1: "))
+    b = int(input("enter num 2: "))
+
+    op = int(input("Select an option:\n1. Add\t2. Sub\t3. Mul\t4. Div\t5. Exit\n"))
+
+    if op == 1:
+        print("Sum is:",a+b)
+    elif op == 2:
+        print("Diff is:", a - b)
+    elif op == 3:
+        print("Product is:", a * b)
+    elif op == 4:
+        print("Quotient is:", a / b)
+    elif op == 5:
+        exit()
+
+
+
+
